3670269384838a10cda376730be3aeed30348b39
Asif Mahmud <asifmahmud3472@gmail.com>
2021-04-22 03:11:37 +0600

    added animation

--- src/components/Home/Services/ServiceItem/ServiceItem.js
+++ src/components/Home/Services/ServiceItem/ServiceItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './ServiceItem.css'
 
 const ServiceItem = ({service, id}) => {
     const {image,title,desc,price} = service;
@@ -7,7 +8,7 @@ const ServiceItem = ({service, id}) => {
         <div className="col-md-5 service-item mt-4 shadow-sm p-3 m-3">
             <div className="row">
                 <div className="col-md-3">
-                    <img src={image} alt=""/>
+                    <img src={image} className="service-img" alt=""/>
                 </div>
                 <div className="col-md-9">
                     <h6>{title}</h6>
--- src/components/Home/TravelRisk/TravelRiskItem/TravelRiskItem.js
+++ src/components/Home/TravelRisk/TravelRiskItem/TravelRiskItem.js
@@ -5,7 +5,9 @@ const TravelRiskItem = ({img,title}) => {
     return (
         <div className="travel-risk-item">
             <div className="col-md-12 text-right">
-                <img src={img} alt=""/>
+                <div className="travel-risk-img text-right">
+                    <img src={img} alt=""/>
+                </div>
                 <h5>{title}</h5>
                 <p>Integer et dapibus nibh. Vestibulum ante ipsum primis faucibus orci.</p>
             </div>
